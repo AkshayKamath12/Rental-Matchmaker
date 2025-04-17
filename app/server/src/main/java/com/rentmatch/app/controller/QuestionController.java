403cd11cafbe352ab815cc745ded180f83773a25
Akshay Kamath <akkamath@ucsc.edu>
2025-04-16 17:54:43 -0700

    fixing some issues with question controller

--- app/server/src/main/java/com/rentmatch/app/controller/QuestionController.java
+++ app/server/src/main/java/com/rentmatch/app/controller/QuestionController.java
@@ -1,27 +1,36 @@
 package com.rentmatch.app.controller;
 
 
+import com.rentmatch.app.dao.UserRepository;
 import com.rentmatch.app.entity.Question;
+import com.rentmatch.app.entity.User;
 import com.rentmatch.app.service.QuestionService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class QuestionController {
     private QuestionService questionService;
+    private UserRepository userRepository;
 
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, UserRepository userRepository) {
         this.questionService = questionService;
+        this.userRepository = userRepository;
     }
 
     private String getLoggedUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null && auth.isAuthenticated()) {
-            return auth.getName();
+            String name = auth.getName();
+            Optional<User> foundUser = userRepository.findByUsernameOrEmail(name, name);
+            if (foundUser.isPresent()) {
+                return foundUser.get().getUsername();
+            }
         }
         return null;
     }
@@ -44,9 +53,16 @@ public class QuestionController {
     @PostMapping("/answers")
     public void setQuestion(@RequestBody Question question) {
         String loggedUser = getLoggedUser();
-        question.setUsername(loggedUser);
         if (loggedUser != null) {
-            questionService.saveQuestion(question);
+            Question dbQuestion = questionService.findQuestion(loggedUser, question.getQuestion());
+            if (dbQuestion != null) {
+                dbQuestion.setAnswer(question.getAnswer());
+                dbQuestion.setWeight(question.getWeight());
+            } else {
+                dbQuestion = new Question(question.getQuestion(), question.getAnswer(), question.getWeight(), loggedUser);
+            }
+
+            questionService.saveQuestion(dbQuestion);
         }
     }
 
