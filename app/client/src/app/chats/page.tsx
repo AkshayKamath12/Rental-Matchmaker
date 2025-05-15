2b2f25d309b0aca2af93154e90dc229e4f086854
Akshay Kamath <akkamath@ucsc.edu>
2025-05-14 19:43:56 -0700

    adding chats page

---
+++ app/client/src/app/chats/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function ChatsPage() {
+  const router = useRouter();
+
+
+
+
+  return (
+    <div className="flex flex-col w-full mx-auto h-screen bg-gradient-to-r from-blue-50 via-purple-50 to-pink-50 p-6 rounded-lg shadow-lg">
+
+    </div>
+  );
+}
